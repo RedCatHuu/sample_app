ab72c20aa40c03dc95b8e479da59fbd10e53142a
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2023-11-03 14:29:25 +0000

    [update]rails cを使うためにgemfileを変更

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,5 +3,6 @@ class List < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true
+  # rails c を使う場合、画像があると複雑になるのでコメントアウトする
   validates :image, presence: true
 end
